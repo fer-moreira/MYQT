399ad23897734c171cbbd9cc4fcd27d587e76705
Fernando Ferreira <34872851+zisongbr@users.noreply.github.com>
2019-01-04 15:02:42 -0200

    Add files via upload

--- Core/SQLManager.py
+++ Core/SQLManager.py
@@ -1,26 +1,22 @@
 from PyQt5 import QtCore
 from PyQt5.QtCore import pyqtSlot,QFile, QTextStream,QCoreApplication
 from PyQt5.uic import loadUi
-
-from PyQt5.QtWidgets import QApplication, QDialog,QFileDialog,QMainWindow,QStyleFactory
-from PyQt5.QtWidgets import QAction,QMenu,QTableWidget,QTableWidgetItem,QTableView
-
 from PyQt5.QtGui import QStandardItemModel,QStandardItem
-
 from PyQt5 import QtGui
-
-
-from Lib.UI.SCRIPT.MainWindow import Ui_SQLMANAGER
-import mysql.connector as mysql
+from PyQt5.QtWidgets import QApplication, QDialog,QFileDialog,QMainWindow,QStyleFactory,QAction,QMenuBar,QAction,QMenu,QTableWidget,QTableWidgetItem
 
 import time
 import sys
+import mysql.connector as mysql
+
+from Lib.UI.SCRIPT.MainWindow import Ui_SQLMANAGER
 
 class ManagerWindow(QMainWindow,QCoreApplication,Ui_SQLMANAGER,object):
     def __init__(self,hs,us,ps,bfr,db, parent = None):
         super(ManagerWindow,self).__init__(parent)
         self.setupUi(self)
-        self.setFixedSize(self.size())        
+        self.setFixedSize(self.size())
+        self.add_menu_bar()
         self.show()
 
         self.hs = hs
@@ -29,12 +25,27 @@ class ManagerWindow(QMainWindow,QCoreApplication,Ui_SQLMANAGER,object):
         self.bfred = bfr
         self.db = db
 
+        self.mydb = mysql.connect(host=self.hs,user=self.us,passwd=self.ps,buffered=self.bfred,database=self.db)
 
-        self.reftable_btn.clicked.connect(self.Get_Tables)        
         self.Get_Tables()
 
-        self.compile_btn.clicked.connect(self.Execute_Querry)
-        self.import_btn.clicked.connect(self.load_query_from_file)
+    def Get_Tables (self):
+        try:
+            self.mycursor = self.mydb.cursor()
+            self.mycursor.execute("show tables")
+            self.myresult = self.mycursor.fetchall()
+
+            tables = []
+
+            for x in self.myresult:
+                tables.append(x[0])
+
+            self.tables_out.clear()
+            self.tables_out.addItems(tables)
+
+        except Exception as error:
+            self.WriteConsole(error)
+            pass
 
     def Execute_Querry (self):
         _query = str(self.query_in.toPlainText()).replace("\n"," ")
@@ -45,7 +56,7 @@ class ManagerWindow(QMainWindow,QCoreApplication,Ui_SQLMANAGER,object):
 
             allSQLRows = cursor.fetchall()
 
-            
+
             lenRow = len(allSQLRows)
             lenCol = len(allSQLRows[0])
 
@@ -61,56 +72,59 @@ class ManagerWindow(QMainWindow,QCoreApplication,Ui_SQLMANAGER,object):
 
 
             columns = cursor.description
-            header_names = []
             for col in range(0,lenCol):
-                print(columns[col][0])
-                header_names.append(columns[col][0])
+                headerName = QTableWidgetItem(columns[col][0])
+                self.result_out.setHorizontalHeaderItem(col, headerName)
 
             self.processEvents()
-            self.tabs.setCurrentIndex(0)
-
 
         except Exception as error:
             self.WriteConsole(error)
             pass
 
-    def Get_Tables (self):
-        try:
-            self.mydb = mysql.connect(
-                host=self.hs,
-                user=self.us,
-                passwd=self.ps,
-                buffered=self.bfred,
-                database=self.db
-            )
 
-            self.mycursor = self.mydb.cursor()
-            self.mycursor.execute("show tables")
-            self.myresult = self.mycursor.fetchall()
-
-            tables = []
-
-            for x in self.myresult:
-                tables.append(x[0])
-
-            self.tables_out.clear()
-            self.tables_out.addItems(tables)            
-            self.tabs.setCurrentIndex(0)
-
-        except Exception as error:
-            self.WriteConsole(error)
-            pass
     def WriteConsole (self,text):
-        now = "{0} {1:<20}".format(time.strftime("%x"),time.strftime("%X"))
-        self.console_out.insertPlainText(str("{0} ERROR:{1}\n".format(now,text)))
-        self.tabs.setCurrentIndex(1)
+        error = "{0} {1:<10} :::   {2}\n".format(time.strftime("%x"),time.strftime("%X"),text)
+        self.console_out.insertPlainText(error)
+        print(error)
 
-        print(text)
-    def load_query_from_file(self):  
-        fname = QFileDialog.getOpenFileName(self, 'Open QUERY file', 'c:\\',"Select query file (*.txt *.sql *.dat *.csv *.tsv *.psv)")        
-        selectFilePath = str(fname[0])
 
-        fileopen = open(r'%s'%selectFilePath,'r')
-        _content = str(fileopen.read())
+    def load_query_from_file(self):
+        try:
+            fname = QFileDialog.getOpenFileName(self, 'Open QUERY file', 'c:\\',"Select query file (*.txt *.sql *.dat *.csv *.tsv *.psv)")
+            selectFilePath = str(fname[0])
 
-        self.query_in.setText(_content)
+            fileopen = open(r'%s'%selectFilePath,'r')
+            _content = str(fileopen.read())
+
+            self.query_in.setText(_content)
+        except FileNotFoundError:pass
+
+    def add_menu_bar (self):
+        mainMenu = self.menuBar()
+
+        importQuery = QAction("&Import Query", self)
+        importQuery.setShortcut("Ctrl+W")
+        importQuery.triggered.connect(self.load_query_from_file)
+
+        exportQuery = QAction("&Export Query",self)
+        exportQuery.setShortcut("Ctrl+S")
+        
+        compileQuery = QAction("&Compile Query",self)
+        compileQuery.setShortcut("F5")
+        compileQuery.triggered.connect(self.Execute_Querry)
+
+        selectedQuery = QAction("&Compile Selected Query",self)
+        selectedQuery.setShortcut("F6")
+        compileQuery.triggered.connect(self.Execute_Querry)
+
+        refreshTables = QAction("&Refresh Tables",self)
+        refreshTables.setShortcut("Ctrl+R")
+        refreshTables.triggered.connect(self.Get_Tables)
+
+        _file = mainMenu.addMenu('&File')
+        _file.addAction(importQuery)
+        _file.addAction(exportQuery)
+        _file.addAction(compileQuery)
+        _file.addAction(selectedQuery)
+        _file.addAction(refreshTables)
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# MYSQL_PYQT
-mysql with python pyqt
+# SQL Manager
+
+SQL Manager é um programa livre e com código-aberto para o MySQL, Microsoft SQL SERVER e PostgreSQL
+
+---
+
+## Detalhes Gerais
+- SQL Manager é desenvolvido em python
+- Desenvolvido por Fernando Moreira entre 2018 - 2019
+
+
+## Alguns detalhes técnicos
+- SQL Manager é desenvolvido em Python
+- Usando o framework PYQT para criação de interfaces (GUI)
+
+</br>
+
+---
+
+**Author : Fernando Ferreira Moreira**</br>
+**CC0 - 2018 Creative Commons**
