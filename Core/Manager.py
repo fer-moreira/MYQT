d5fccd0a9ca695b3df70335c031d409cd5632b18
Fernando Ferreira <34872851+zisongbr@users.noreply.github.com>
2019-01-10 15:05:17 -0200

    Add files via upload

--- Core/Manager.py
+++ Core/Manager.py
@@ -1,14 +1,12 @@
 from PyQt5 import QtCore
 from PyQt5.uic import loadUi
 
-from PyQt5 import QtGui
 from PyQt5.QtGui import QStandardItemModel,QStandardItem
 
 from PyQt5.QtWidgets import QApplication, QDialog,QFileDialog,QMainWindow,QStyleFactory
-from PyQt5.QtWidgets import QAction,QToolBar,QTableWidget,QTableWidgetItem
+from PyQt5.QtWidgets import QAction,QToolBar,QTableWidget,QTableWidgetItem,QHeaderView
 
-from PyQt5.QtCore import pyqtSlot,QFile, QTextStream,QCoreApplication
-
+from PyQt5.QtCore import pyqtSlot,QFile, QTextStream,QCoreApplication,Qt
 from PyQt5.QtGui import QIcon
 
 import time
@@ -18,8 +16,7 @@ from functools import partial
 
 from Lib.UI.SCRIPT.MainWindow import Ui_SQLMANAGER
 
-
-class ManagerWindow(QMainWindow,QCoreApplication,Ui_SQLMANAGER,object):
+class ManagerWindow(QMainWindow,QToolBar,QCoreApplication,Ui_SQLMANAGER,object):
     def __init__(self,hs,us,ps,bfr,db, parent = None):
         super(ManagerWindow,self).__init__(parent)
         self.setupUi(self)
@@ -44,7 +41,12 @@ class ManagerWindow(QMainWindow,QCoreApplication,Ui_SQLMANAGER,object):
         self.tables_out.itemSelectionChanged.connect(self.Get_Desc)
         self.tables_out.itemSelectionChanged.connect(self.Get_All_Data)
 
-
+        self.tables_out.itemDoubleClicked.connect(self.AppendSelected)
+    def AppendSelected (self):
+        table = str(self.tables_out.currentItem().text())
+        self.query_in.insertPlainText(table)
+    
+    #### CODE EXECUTE HADLER      #################################################################################################################
     def Execute_Querry (self,text):
         _query = text
 
@@ -72,19 +74,18 @@ class ManagerWindow(QMainWindow,QCoreApplication,Ui_SQLMANAGER,object):
                 headerName = QTableWidgetItem(columns[col][0])
                 self.result_out.setHorizontalHeaderItem(col, headerName)
 
-            self.processEvents()
-            self.console_out.setPlainText("")
+            self.result_out.resizeColumnsToContents()
 
-            self.WriteConsole("Query OK!",False)
+            self.WriteConsole(_query)
 
         except Exception as error:
-            self.WriteConsole(error,True)
+            self.WriteConsole(error)
             pass
-
+    #### RUN ALL CODE IN CONSULT  #################################################################################################################
     def GetAllQuery (self):
         _allQuery = str(self.query_in.toPlainText()).replace("\n"," ")
-        self.Execute_Querry(_allQuery)
-    
+        self.Execute_Querry(_allQuery) 
+    #### RUN SELECTED QUERY IN CONSULT ############################################################################################################
     def GetSelectedQuery (self):
         cursor = self.query_in.textCursor()
         
@@ -98,7 +99,7 @@ class ManagerWindow(QMainWindow,QCoreApplication,Ui_SQLMANAGER,object):
             _buildText+=_allText[letter]
 
         self.Execute_Querry(_buildText)
-
+    #### GET ALL TABLES FROM DATA BASE ############################################################################################################
     def Get_Tables (self):
         try:
             self.mycursor = self.mydb.cursor()
@@ -114,9 +115,9 @@ class ManagerWindow(QMainWindow,QCoreApplication,Ui_SQLMANAGER,object):
             self.tables_out.addItems(tables)
         
         except Exception as error:
-            self.WriteConsole(error,True)
+            self.WriteConsole(error)
             pass
-    
+    #### SHOW COLUMNS CREATION INFO IN TABLE  #####################################################################################################
     def Get_Desc (self):
         table = self.tables_out.currentItem().text()
         _query = "desc %s"%table
@@ -144,12 +145,14 @@ class ManagerWindow(QMainWindow,QCoreApplication,Ui_SQLMANAGER,object):
                 headerName = QTableWidgetItem(columns[col][0])
                 self.desc_result.setHorizontalHeaderItem(col, headerName)
 
+            self.desc_result.resizeColumnsToContents()
+            self.WriteConsole(str(_query))
             self.processEvents()
 
         except Exception as error:
-            self.WriteConsole(error,True)
+            self.WriteConsole(error)
             pass
-
+    #### DESCRIBE ALL DATA IN SELECTED TABLE  #####################################################################################################
     def Get_All_Data (self):
         table = self.tables_out.currentItem().text()
         _query = "select* from %s"%table
@@ -164,7 +167,7 @@ class ManagerWindow(QMainWindow,QCoreApplication,Ui_SQLMANAGER,object):
             lenCol = len(allSQLRows[0])
 
             self.data_result.setRowCount(lenRow) ##set number of rows
-            self.data_result.setColumnCount(lenCol) ##this is fixed for result_out, ensure that both of your tables, sql and qtablewidged have the same number of columns
+            self.data_result.setColumnCount(lenCol) ##set number of columns 
 
             if allSQLRows is not None:
                 for lin in range(0,lenRow):
@@ -177,69 +180,57 @@ class ManagerWindow(QMainWindow,QCoreApplication,Ui_SQLMANAGER,object):
                 headerName = QTableWidgetItem(columns[col][0])
                 self.data_result.setHorizontalHeaderItem(col, headerName)
             
+            self.data_result.resizeColumnsToContents()
+            self.WriteConsole(str(_query))
             self.processEvents()
-
         except Exception as error:
-            self.WriteConsole(error,True)
+            self.WriteConsole(error)
             pass
-
-    def WriteConsole (self,text,isError):
-        if isError:
-            error = "{0} {1:<10} :::   {2}\n".format(time.strftime("%x"),time.strftime("%X"),text)
-
-            self.console_out.setStyleSheet("color: rgb(255, 255, 255); background-color: rgb(79, 0, 1);padding:10;")
-            self.console_out.setPlainText(error)
-        else:
-            self.console_out.setStyleSheet("color: rgb(255, 255, 255); background-color: rgb(35, 68, 0);padding:10;")
-            self.console_out.setPlainText(text)
-
+    #### WRITE ALL ACTIONS IN CONSOLE OUTPUT  #####################################################################################################
+    def WriteConsole (self,text):
+        self.console_out.appendPlainText(str(text))
+        self.processEvents()
+    #### TOOL BAR CREATION HANDLER  ###############################################################################################################
     def add_tool_bar(self):
-        print("TOOL BAR")
+        toolBar = QToolBar()
+        toolBar.setMovable(False)
+        tb_config = self.addToolBar(Qt.LeftToolBarArea,toolBar)
+        # tb = self.addToolBar("File")
 
-        tb = self.addToolBar("File")
+        ############### CREATING BUTTONS ICONS AND SETTING FUNCTION #############################################################################################################
+        compileAll =        QAction(QIcon(r'src\Toolbar_Icons\compile.PNG'),    "Execute SQL",          self,shortcut="F9",             triggered=self.GetAllQuery)              #
+        compileSelected =   QAction(QIcon(r'src\Toolbar_Icons\compileSel.PNG'), "Execute Selected",     self,shortcut="Shift+Ctrl+F9",  triggered=self.GetSelectedQuery)         #
+        import_t =          QAction(QIcon(r'src\Toolbar_Icons\import.PNG'),     "Load SQL from file",   self,shortcut="Ctrl+O",         triggered=self.load_query_from_file)     #
+        export_t =          QAction(QIcon(r'src\Toolbar_Icons\export.PNG'),     "Save SQL in file",     self,shortcut="Ctrl+S",         triggered=self.save_query_to_file)       #
+        refresh_t =         QAction(QIcon(r'src\Toolbar_Icons\refresh.PNG'),    "Refresh",              self,shortcut="F5",             triggered=self.Get_Tables)               #
+        #########################################################################################################################################################################
 
-        compileAll = QAction(QIcon(r'src\Toolbar_Icons\compile.PNG'),"Compile All",self)
-        compileAll.triggered.connect(self.GetAllQuery)
-
-        compileSelected = QAction(QIcon(r'src\Toolbar_Icons\compileSel.PNG'),"Compile Selected",self)
-        compileSelected.triggered.connect(self.GetSelectedQuery)
-
-        import_t = QAction(QIcon(r'src\Toolbar_Icons\import.PNG'),"Import Query",self)
-        import_t.triggered.connect(self.load_query_from_file)
-
-        export_t = QAction(QIcon(r'src\Toolbar_Icons\export.PNG'),"Export Query",self)
-        export_t.triggered.connect(self.save_query_to_file)
-
-        refresh_t = QAction(QIcon(r'src\Toolbar_Icons\refresh.PNG'),"Refresh Tables",self)
-        refresh_t.triggered.connect(self.Get_Tables)
-
-
-        tb.addAction(refresh_t)
-        tb.addAction(compileAll)
-        tb.addAction(compileSelected)
-        tb.addAction(import_t)
-        tb.addAction(export_t)
-
+        ## ADDING BUTTON TO MENU ################
+        toolBar.addAction(refresh_t)             #
+        toolBar.addSeparator()                   #
+        toolBar.addAction(compileAll)            #
+        toolBar.addAction(compileSelected)       #
+        toolBar.addSeparator()                   #
+        toolBar.addAction(import_t)              #
+        toolBar.addAction(export_t)              #
+        #########################################
+    #### LOAD QUERY FROM FILE IN SYSTEM ###########################################################################################################
     def load_query_from_file(self):
         try:
-            fname = QFileDialog.getOpenFileName(self, 'Open QUERY file', 'Query',"Select query file (*.txt *.sql *.dat *.csv *.tsv *.psv)")
+            fname = QFileDialog.getOpenFileName(self, 'Load SQL From file', 'Query',"Select query file (*.txt *.sql *.dat *.csv *.tsv *.psv)")
             selectFilePath = str(fname[0])
-
             fileopen = open(r'%s'%selectFilePath,'r')
             _content = str(fileopen.read())
-
             self.query_in.setPlainText(_content)
         except FileNotFoundError:pass
-
+    #### SAVE QUERY TO FILE #######################################################################################################################
     def save_query_to_file(self):
         try:
             options = QFileDialog.Options()
-            saved_file,_ = QFileDialog.getSaveFileName(self,"Export Query","Query","Query Files (*.sql);;Text Files (*.txt);;Data Files (*.dat);;All Files (*)",options=options)
-            
+            saved_file,_ = QFileDialog.getSaveFileName(self,"Save SQL to file Query","Query","Query Files (*.sql);;Text Files (*.txt);;Data Files (*.dat);;All Files (*)",options=options)            
             toSave_query = self.query_in.toPlainText()
-
             _file = open(saved_file,'w')
             _file.write(toSave_query)
             _file.close()
         except FileNotFoundError:pass
-
+    ###############################################################################################################################################
